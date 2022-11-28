5fed1dc5daa11447b0e3281e21858de30389b32a
Pawel Osuch <paosuch@gmail.com>
2022-11-28 21:11:53 +0000

    slider is ready

--- app.js
+++ app.js
@@ -3,14 +3,6 @@ let activeSlideNumber = 1;
 let arrowLeft = document.querySelector(".arrow-left");
 let arrowRight = document.querySelector(".arrow-right");
 
-let dot1 = document.querySelector("#dot1");
-let dot2 = document.querySelector("#dot2");
-let dot3 = document.querySelector("#dot3");
-
-let slide1 = document.querySelector("#slide1");
-let slide2 = document.querySelector("#slide2");
-let slide3 = document.querySelector("#slide3");
-
 let hideActiceSlide = () => {
     let activeElement = document.querySelector (".active");
     activeElement.classList.remove("active");
@@ -41,23 +33,13 @@ let showPreviousSlide = () => {
     showSlide(activeSlideNumber);
 }
 
-let showSlide1 = () => {
-    activeSlideNumber = 1;
-    showSlide(1);
-};
-
-let showSlide2 = () => {
-    activeSlideNumber = 2;
-    showSlide(2);
-};
-
-let showSlide3 = () => {
-    activeSlideNumber = 3;
-    showSlide(3);
-};
-dot1.addEventListener("click", showSlide1);
-dot2.addEventListener("click", showSlide2);
-dot3.addEventListener("click", showSlide3);
+for(let i = 1; i <=3; i++) {
+    let showSlideI = () => {
+        activeSlideNumber = i;
+        showSlide(i);
+    }
+    document.querySelector("#dot"+i).addEventListener('click', showSlideI);
+}
 
 arrowRight.addEventListener("click", showNextSlide);
 arrowLeft.addEventListener("click", showPreviousSlide);
--- example.js
+++ example.js
@@ -1,23 +1,7 @@
-let bool = 5 > 2;
-console.log(bool);
-
-let bool1 = 1 === 1;
-console.log(bool1);
-
-let bool2 = 1 !==2;
-console.log(bool2);
-
-let bool3 = "Pawel" === "Jowita";
-console.log(bool3);
-
-let isBiggerThan10 = (number) => {
-    if(number > 10) {
-        console.log("Liczba jest większa od 10")
-    }
-
-    else{
-        console.log("Liczba nie jest większa  od 10")
-    }
+let circleLength = (radius) => {
+    console.log(2*radius*3.14);
 };
-isBiggerThan10 (20)
-isBiggerThan10 (4)
+
+for(let i =1; i <=20; i++) {
+    circleLength(i);
+}
